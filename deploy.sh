71c7f137c3d7acf98f17d43b5d46a3d6c06176a3
Jacob Boswell <jboswell@monarx.com>
2019-08-05 20:12:26 -0600

    changed region

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-repository="542516710673.dkr.ecr.eu-central-1.amazonaws.com"
+repository="542516710673.dkr.ecr.us-west-2.amazonaws.com"
 
 $(aws ecr get-login --no-include-email)
 docker build . -t beanstalkd:latest -t $repository/beanstalkd:latest
